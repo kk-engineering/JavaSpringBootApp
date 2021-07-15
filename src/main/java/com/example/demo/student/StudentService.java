e9978fd35ed8e00f918a3310508311b5331ee28b
Med Kaikai <medkaikai@gmail.com>
2021-07-15 16:24:28 +0100

    Initial Commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import com.example.demo.student.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
+}
